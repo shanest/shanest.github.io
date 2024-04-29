43b94019ca53b394e8ae83fca93a79c859e1dc66
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-04-29 11:05:27 -0700

    init commit of 11ty version

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Shane Steinert-Threlkeld",
+	url: "https://www.shane.st/",
+	language: "en",
+	description: "Personal website of Shane Steinert-Threlkeld, with information on research and teaching, as well as a blog and trip reports from various outdoor adventures.",
+	author: {
+		name: "Shane Steinert-Threlkeld",
+		email: "ssshanest@gmail.com",
+		url: "https://www.shane.st/"
+	}
+}
