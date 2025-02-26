3c4ba5e0a287441bc294cde971dfbf50a4893791
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2025-02-26 10:41:58 -0800

    typographer for em/en dashes

--- eleventy.config.js
+++ eleventy.config.js
@@ -115,7 +115,8 @@ export default async function(eleventyConfig) {
         {
             html: true,
             // breaks: true,
-            linkify: true
+            linkify: true,
+            typographer: true,
         }
     )
 
