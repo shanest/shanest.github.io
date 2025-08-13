cc4ee6c224d17d9b110a1822e80e599975c87567
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2025-08-13 14:39:13 -0700

    remove feed from navbar

--- eleventy.config.js
+++ eleventy.config.js
@@ -62,12 +62,14 @@ export default async function(eleventyConfig) {
 		type: "atom", // or "rss", "json"
 		outputPath: "/feed/feed.xml",
 		stylesheet: "pretty-atom-feed.xsl",
+		/*
 		templateData: {
 			eleventyNavigation: {
 				key: "Feed",
 				order: 4
 			}
 		},
+		*/
 		collection: {
 			name: "posts",
 			limit: 10,
